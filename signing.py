dd754acd42de7f71c55bfc2579930de300c7b97d
FalseFate <82357000+FalseFate@users.noreply.github.com>
2021-04-12 00:12:36 -0400

    Create signing.py

---
+++ signing.py
@@ -0,0 +1,17 @@
+import discrd
+from discord.ext import commands
+
+
+
+bot = commands.Bot(command_prefix="?")
+
+
+
+@bot.command()
+async def sign(ctx, user:discord.Member=None):
+  
+                   
+                   
+         
+                   
+   
